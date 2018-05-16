b1a5a3fabc1ac5a8d8b77853195f36977d07e69f
Brian Anderson <andersrb@gmail.com>
2018-05-16 15:02:14 -0700

    .

--- src/lib.rs
+++ src/lib.rs
@@ -4,7 +4,8 @@
 extern crate test;
 #[macro_use]
 extern crate jetscii;
-//extern crate faster;
+extern crate faster;
+extern crate memchr;
 
 use test::Bencher;
 use jetscii::Bytes;
@@ -20,53 +21,77 @@ static EXAMPLE_LIPSUM: &str = include_str!("lipsum.md");
 static EXAMPLE_LIPSUM_BR: &str = include_str!("lipsum-linebreaks.md");
 static EXAMPLE_LIPSUM_EMPH: &str = include_str!("lipsum-emph.md");
 
-/*#[test]
-fn find_from_set_jetscii_bytes_lipsum_emph() {
-    let bytes = bytes!(b'#', b'_', b'*', b'=', b'-', b'~', b'|', b'[', b'\\', b'>', b'^', b'`', b'&', b'/', b':', b'@');
-    for i in 0..100 {
-    let r = bytes.find(EXAMPLE_LIPSUM_EMPH.as_bytes());
-        assert_eq!(r, Some(419));
+mod find_set_of_bytes {
+
+    use super::*;
+
+    #[bench]
+    fn find_from_set_jetscii_bytes_lipsum_emph(b: &mut Bencher) {
+        let bytes = bytes!(b'#', b'_', b'*', b'=', b'-', b'~', b'|', b'[', b'\\', b'>', b'^', b'`', b'&', b'/', b':', b'@');
+        b.iter(|| {
+            let r = bytes.find(EXAMPLE_LIPSUM_EMPH.as_bytes());
+            assert_eq!(r, Some(419));
+            black_box(r);
+        });
+    }
+
+    #[bench]
+    fn find_from_set_jetscii_ascii_lipsum_emph(b: &mut Bencher) {
+        let chars = ascii_chars!('#', '_', '*', '=', '-', '~', '|', '[', '\\', '>', '^', '`', '&', '/', ':', '@');
+        b.iter(|| {
+            let r = chars.find(EXAMPLE_LIPSUM_EMPH);
+            assert_eq!(r, Some(419));
+            black_box(r);
+        });
+    }
+
+    #[bench]
+    fn find_from_set_open_table_lipsum_emph(b: &mut Bencher) {
+        let mut table: [bool; 256] = [false; 256];
+        for ch in FORBIDDEN_CHARS {
+            table[*ch as usize] = true;
+        }
+        let table = table;
+        b.iter(|| {
+            let mut r = None;
+            for (i, byte) in EXAMPLE_LIPSUM_EMPH.as_bytes().iter().enumerate() {
+                if table[*byte as usize] {
+                    r = Some(i);
+                    break;
+                }
+            }
+            assert_eq!(r, Some(419));
+            black_box(r);
+        });
+    }
+
+    #[bench]
+    fn find_from_set_position_table_lipsum_emph(b: &mut Bencher) {
+        let mut table: [bool; 256] = [false; 256];
+        for ch in FORBIDDEN_CHARS {
+            table[*ch as usize] = true;
+        }
+        let table = table;
+        b.iter(|| {
+            let r = EXAMPLE_LIPSUM_EMPH.as_bytes().iter().position(|byte| {
+                if table[*byte as usize] {
+                    true
+                } else {
+                    false
+                }
+            });
+            assert_eq!(r, Some(419));
+            black_box(r);
+        });
+    }
+
+
+    #[bench]
+    fn find_from_set_multi_memchr_table_lipsum_emph(b: &mut Bencher) {
     }
 }
 
-#[test]
-fn find_from_set_jetscii_ascii_lipsum_emph() {
-    let chars = ascii_chars!(b'#', b'_', b'*', b'=', b'-', b'~', b'|', b'[', b'\\', b'>', b'^', b'`', b'&', b'/', b':', b'@');
-    let r = chars.find(EXAMPLE_LIPSUM_EMPH);
-    assert_eq!(r, Some(419));
-}*/
-
-#[bench]
-fn find_from_set_jetscii_bytes_lipsum_emph(b: &mut Bencher) {
-    let bytes = bytes!(b'#', b'_', b'*', b'=', b'-', b'~', b'|', b'[', b'\\', b'>', b'^', b'`', b'&', b'/', b':', b'@');
-    b.iter(|| {
-        let r = bytes.find(EXAMPLE_LIPSUM_EMPH.as_bytes());
-        assert_eq!(r, Some(419));
-        black_box(r);
-    });
-}
-
-/*#[bench]
-fn find_from_set_jetscii_ascii_lipsum_emph(b: &mut Bencher) {
-    let chars = ascii_chars!(b'#', b'_', b'*', b'=', b'-', b'~', b'|', b'[', b'\\', b'>', b'^', b'`', b'&', b'/', b':', b'@');
-    b.iter(|| {
-        let r = chars.find(EXAMPLE_LIPSUM_EMPH);
-        assert_eq!(r, Some(419));
-        black_box(r);
-    });
-}
-
 // TODO: benchmark chars creation
-
-#[bench]
-fn find_from_set_table_lipsum_emph(b: &mut Bencher) {
-    let table: [u8; 256] = [0; 256];
-    b.iter(|| {
-        for byte in EXAMPLE_LIPSUM_EMPH.as_bytes() {
-
-        }
-    });
-}
 
 #[bench]
 fn find_substring_std(b: &mut Bencher) {
@@ -83,4 +108,4 @@ fn is_ascii_faster(b: &mut Bencher) {
 #[bench]
 fn line_split_std(b: &mut Bencher) {
 }
-*/
+
