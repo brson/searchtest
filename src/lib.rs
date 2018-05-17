9041e5eac9a93da33dc2ecc666a761e36d6d3b01
Brian Anderson <andersrb@gmail.com>
2018-05-16 22:08:32 -0700

    .

--- src/lib.rs
+++ src/lib.rs
@@ -10,6 +10,8 @@ extern crate memchr;
 use test::Bencher;
 use jetscii::Bytes;
 use test::black_box;
+use memchr::*;
+use jetscii::ByteSubstring;
 
 
 static FORBIDDEN_CHARS: &[u8] = &[b'#', b'_', b'*', b'=', b'-', b'~', b'|', b'[', b'\\', b'>', b'^', b'`', b'&', b'/', b':', b'@'];
@@ -20,8 +22,9 @@ static EXAMPLE_WWW: &str = include_str!("www.md");
 static EXAMPLE_LIPSUM: &str = include_str!("lipsum.md");
 static EXAMPLE_LIPSUM_BR: &str = include_str!("lipsum-linebreaks.md");
 static EXAMPLE_LIPSUM_EMPH: &str = include_str!("lipsum-emph.md");
+static EXAMPLE_LIPSUM_AT: &str = include_str!("lipsum-at.md");
 
-mod find_set_of_bytes {
+mod find_set_of_bytes_early {
 
     use super::*;
 
@@ -30,6 +33,8 @@ mod find_set_of_bytes {
         let bytes = bytes!(b'#', b'_', b'*', b'=', b'-', b'~', b'|', b'[', b'\\', b'>', b'^', b'`', b'&', b'/', b':', b'@');
         b.iter(|| {
             let r = bytes.find(EXAMPLE_LIPSUM_EMPH.as_bytes());
+            assert!(r.is_some());
+            assert_eq!(EXAMPLE_LIPSUM_EMPH.as_bytes()[r.unwrap()] as char, '_');
             assert_eq!(r, Some(419));
             black_box(r);
         });
@@ -40,6 +45,8 @@ mod find_set_of_bytes {
         let chars = ascii_chars!('#', '_', '*', '=', '-', '~', '|', '[', '\\', '>', '^', '`', '&', '/', ':', '@');
         b.iter(|| {
             let r = chars.find(EXAMPLE_LIPSUM_EMPH);
+            assert!(r.is_some());
+            assert_eq!(EXAMPLE_LIPSUM_EMPH.as_bytes()[r.unwrap()] as char, '_');
             assert_eq!(r, Some(419));
             black_box(r);
         });
@@ -60,6 +67,8 @@ mod find_set_of_bytes {
                     break;
                 }
             }
+            assert!(r.is_some());
+            assert_eq!(EXAMPLE_LIPSUM_EMPH.as_bytes()[r.unwrap()] as char, '_');
             assert_eq!(r, Some(419));
             black_box(r);
         });
@@ -80,26 +89,150 @@ mod find_set_of_bytes {
                     false
                 }
             });
+            assert!(r.is_some());
+            assert_eq!(EXAMPLE_LIPSUM_EMPH.as_bytes()[r.unwrap()] as char, '_');
             assert_eq!(r, Some(419));
             black_box(r);
         });
     }
 
-
     #[bench]
-    fn find_from_set_multi_memchr_table_lipsum_emph(b: &mut Bencher) {
+    fn find_from_set_multi_memchr_lipsum_emph(b: &mut Bencher) {
         b.iter(|| {
             let mut r = None;
-            for window in FORBIDDEN_CHARS.as_bytes().chunks(3) {
-                match window.len() {
-                    1 => {
-                    }
-                    2 => {
-                    }
-                    3 => {
-                    }
+            for window in FORBIDDEN_CHARS.chunks(3) {
+                let r_ = match window {
+                    [a, b, c] => memchr3(*a, *b, *c, EXAMPLE_LIPSUM_EMPH.as_bytes()),
+                    [a, b] => memchr2(*a, *b, EXAMPLE_LIPSUM_EMPH.as_bytes()),
+                    [a] => memchr(*a, EXAMPLE_LIPSUM_EMPH.as_bytes()),
+                    _ => unreachable!(),
+                };
+                if r_.is_some() {
+                    r = r_;
+                    break;
                 }
             }
+            assert!(r.is_some());
+            assert_eq!(EXAMPLE_LIPSUM_EMPH.as_bytes()[r.unwrap()] as char, '_');
+            assert_eq!(r, Some(419));
+            black_box(r);
+        });
+    }
+
+    #[bench]
+    fn find_from_set_multi_jump_table_lipsum_emph(b: &mut Bencher) {
+        b.iter(|| {
+            let mut r = None;
+            for (i, ch) in EXAMPLE_LIPSUM_EMPH.as_bytes().iter().enumerate() {
+                match ch {
+                    b'#' |  b'_' |  b'*' |  b'=' |  b'-' |  b'~' |  b'|' |  b'[' |
+                    b'\\' | b'>' |  b'^' |  b'`' |  b'&' |  b'/' |  b':' |  b'@' => {
+                        r = Some(i);
+                        break
+                    }
+                    _ => ()
+                }
+            }
+            assert!(r.is_some());
+            assert_eq!(EXAMPLE_LIPSUM_EMPH.as_bytes()[r.unwrap()] as char, '_');
+            assert_eq!(r, Some(419));
+            black_box(r);
+        });
+    }
+    
+}
+
+mod find_set_of_bytes_late {
+
+    use super::*;
+
+    #[bench]
+    fn find_from_set_jetscii_bytes_lipsum_at(b: &mut Bencher) {
+        let bytes = bytes!(b'#', b'_', b'*', b'=', b'-', b'~', b'|', b'[', b'\\', b'>', b'^', b'`', b'&', b'/', b':', b'@');
+        b.iter(|| {
+            let r = bytes.find(EXAMPLE_LIPSUM_AT.as_bytes());
+            assert!(r.is_some());
+            assert_eq!(EXAMPLE_LIPSUM_AT.as_bytes()[r.unwrap()] as char, '@');
+            assert_eq!(r, Some(610));
+            black_box(r);
+        });
+    }
+
+    #[bench]
+    fn find_from_set_jetscii_ascii_lipsum_at(b: &mut Bencher) {
+        let chars = ascii_chars!('#', '_', '*', '=', '-', '~', '|', '[', '\\', '>', '^', '`', '&', '/', ':', '@');
+        b.iter(|| {
+            let r = chars.find(EXAMPLE_LIPSUM_AT);
+            assert!(r.is_some());
+            assert_eq!(EXAMPLE_LIPSUM_AT.as_bytes()[r.unwrap()] as char, '@');
+            assert_eq!(r, Some(610));
+            black_box(r);
+        });
+    }
+    
+    #[bench]
+    fn find_from_set_position_table_lipsum_at(b: &mut Bencher) {
+        let mut table: [bool; 256] = [false; 256];
+        for ch in FORBIDDEN_CHARS {
+            table[*ch as usize] = true;
+        }
+        let table = table;
+        b.iter(|| {
+            let r = EXAMPLE_LIPSUM_AT.as_bytes().iter().position(|byte| {
+                if table[*byte as usize] {
+                    true
+                } else {
+                    false
+                }
+            });
+            assert!(r.is_some());
+            assert_eq!(EXAMPLE_LIPSUM_AT.as_bytes()[r.unwrap()] as char, '@');
+            assert_eq!(r, Some(610));
+            black_box(r);
+        });
+    }
+
+    #[bench]
+    fn find_from_set_multi_memchr_lipsum_at(b: &mut Bencher) {
+        b.iter(|| {
+            let mut r = None;
+            for chunk in FORBIDDEN_CHARS.chunks(3) {
+                let r_ = match chunk {
+                    [a, b, c] => memchr3(*a, *b, *c, EXAMPLE_LIPSUM_AT.as_bytes()),
+                    [a, b] => memchr2(*a, *b, EXAMPLE_LIPSUM_AT.as_bytes()),
+                    [a] => memchr(*a, EXAMPLE_LIPSUM_AT.as_bytes()),
+                    _ => unreachable!(),
+                };
+                if r_.is_some() {
+                    r = r_;
+                    break;
+                }
+            }
+            assert!(r.is_some());
+            assert_eq!(EXAMPLE_LIPSUM_AT.as_bytes()[r.unwrap()] as char, '@');
+            assert_eq!(r, Some(610));
+            black_box(r);
+        });
+    }
+
+    #[bench]
+    fn find_from_set_multi_jump_table_lipsum_at(b: &mut Bencher) {
+        b.iter(|| {
+            let mut r = None;
+            for (i, ch) in EXAMPLE_LIPSUM_AT.as_bytes().iter().enumerate() {
+                match ch {
+                    b'#' |  b'_' |  b'*' |  b'=' |  b'-' |  b'~' |  b'|' |  b'[' |
+                    b'\\' | b'>' |  b'^' |  b'`' |  b'&' |  b'/' |  b':' |  b'@' => {
+                        r = Some(i);
+                        break
+                    }
+                    _ => ()
+                }
+            }
+            assert!(r.is_some());
+            assert_eq!(EXAMPLE_LIPSUM_AT.as_bytes()[r.unwrap()] as char, '@');
+            assert_eq!(r, Some(610));
+            black_box(r);
         });
     }
 }
@@ -113,19 +246,52 @@ fn jetscii_setup(b: &mut Bencher) {
     });
 }
 
-#[bench]
-fn find_substring_std(b: &mut Bencher) {
+mod find_substring {
+
+    use super::*;
+
+    #[bench]
+    fn find_substring_std(b: &mut Bencher) {
+        b.iter(|| {
+            let r = EXAMPLE_WWW.find("www.");
+            assert!(r.is_some());
+            assert_eq!(EXAMPLE_WWW.as_bytes()[r.unwrap()] as char, 'w');
+            assert_eq!(r, Some(599));
+        });
+    }
+
+    #[bench]
+    fn find_substring_jetscii(b: &mut Bencher) {
+        let sub = ByteSubstring::new("www.".as_bytes());
+        b.iter(|| {
+            let r = sub.find(EXAMPLE_WWW.as_bytes());
+            assert!(r.is_some());
+            assert_eq!(EXAMPLE_WWW.as_bytes()[r.unwrap()] as char, 'w');
+            assert_eq!(r, Some(599));
+        });
+    }
 }
 
-#[bench]
-fn is_ascii_std(b: &mut Bencher) {
+mod is_ascii {
+
+    use super::*;
+
+    #[bench]
+    fn is_ascii_std(b: &mut Bencher) {
+    }
+
+    #[bench]
+    fn is_ascii_faster(b: &mut Bencher) {
+    }
+
+    #[bench]
+    fn line_split_std(b: &mut Bencher) {
+    }
+
 }
 
-#[bench]
-fn is_ascii_faster(b: &mut Bencher) {
-}
+mod split_newlines {
 
-#[bench]
-fn line_split_std(b: &mut Bencher) {
-}
+    use super::*;
 
+}
--- src/www.md
+++ src/www.md
@@ -1,1 +1,8 @@
-This is a document with a plan www.example.com link in it.
+Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean tempor turpis
+eget ipsum mattis fermentum. Etiam consequat nisi non sapien finibus lobortis.
+Nulla at pulvinar mi. Suspendisse velit lectus, auctor eget dictum eu,
+pellentesque molestie velit. Sed eget cursus diam, pharetra interdum lacus. Duis
+non odio ac libero lacinia euismod. Praesent ornare nunc in nisi placerat
+semper. Integer eget faucibus eros. Praesent eget tempor ex. Vivamus sit amet
+placerat purus. Aenean lobortis ante diam, scelerisque sodales ex faucibus vel.
+Aenean sed vulputate urna. This is a document with a plan www.example.com link in it.
